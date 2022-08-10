3c04c91220d9af9d7281be95a324f023c8525467
Gareth Williams <gaz492@gmail.com>
2022-08-10 23:43:56 +0100

    Ill do it this way then

--- upx.sh
+++ upx.sh
@@ -2,9 +2,6 @@
 
 array=(windows.exe windows-arm.exe linux linux-arm mac mac-arm)
 
-wget https://github.com/upx/upx/releases/download/v3.96/upx-3.96-amd64_linux.tar.xz
-tar -xf upx-3.96-amd64_linux.tar.xz "upx-3.96-amd64_linux/upx" --strip-components=1
-
 for i in "${array[@]}"; do
   ./upx "test/$i"
 done
