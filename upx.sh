3fdb3afe6a4bcc5ff24cd2a26eecdfcabd5e0e75
Gareth Williams <gaz492@gmail.com>
2022-08-10 23:41:16 +0100

    this?

--- upx.sh
+++ upx.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 array=(windows.exe windows-arm.exe linux linux-arm mac mac-arm)
-#array=(windows.exe)
+
+wget https://github.com/upx/upx/releases/download/v3.96/upx-3.96-amd64_linux.tar.xz
+tar -xf upx-3.96-amd64_linux.tar.xz "upx-3.96-amd64_linux/upx" --strip-components=1
+
 for i in "${array[@]}"; do
   ./upx "test/$i"
 done
